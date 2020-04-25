e622f8e23b1730023785cadd30ada40e624bc52c
Pablo Gaeta <pablofgaeta@gmail.com>
2020-04-25 01:03:35 -0700

    Sin control over cloned rectangles

---
+++ Endless2D/Assets/Scripts/Background.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public int height;
+    public int width;
+    public int fidelity;
+
+    public GameObject go;
+
+    private GameObject[] gos;
+
+    void Start()
+    {
+        gos = new GameObject[fidelity + 1];
+
+        for (int i = 0; i <= fidelity; i++) {
+            gos[i] = Instantiate(go, new Vector3(-10 + i * ((float)fidelity) / 5.0f, -5, 0), Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i <= fidelity; i++) {
+            float yOffset = (Mathf.Sin(Time.time / 1.0f + i * Mathf.PI / 5.0f) + 1.0f);
+            gos[i].transform.localScale = new Vector3(1000.0f / ((float)fidelity), yOffset * height, 0);
+        }
+    }
+
+}
+
