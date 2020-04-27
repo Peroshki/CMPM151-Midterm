45cc31927465e334d432eacb8025758fbadc6fdf
Pablo Gaeta <pablofgaeta@gmail.com>
2020-04-26 20:03:49 -0700

    Set up 2 songs. Just need transition

--- Endless2D/Assets/Scripts/PdHandler.cs
+++ Endless2D/Assets/Scripts/PdHandler.cs
@@ -18,20 +18,28 @@ public class PdHandler : MonoBehaviour
 
 		//************* Instantiate the OSC Handler...
 	    OSCHandler.Instance.Init ();
+		OSCHandler.Instance.SendMessageToClient("pd", "/unity/start", 1);
         //*************
     }
 
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.JoystickButton0)) {
 			Debug.Log("A pressed");
-			OSCHandler.Instance.SendMessageToClient("pd", "/unity/start", 1);
+			OSCHandler.Instance.SendMessageToClient("pd", "/unity/A", 1);
 		}
 		else if (Input.GetKeyDown(KeyCode.JoystickButton1)) {
 			Debug.Log("B pressed");
-			OSCHandler.Instance.SendMessageToClient("pd", "/unity/death", 1);
+			OSCHandler.Instance.SendMessageToClient("pd", "/unity/B", 1);
 		}
-		else if (Input.GetKeyDown(KeyCode.JoystickButton2)) Debug.Log("X pressed");
-		else if (Input.GetKeyDown(KeyCode.JoystickButton3)) Debug.Log("Y pressed");
+		else if (Input.GetKeyDown(KeyCode.JoystickButton2)) {
+			Debug.Log("X pressed");
+			OSCHandler.Instance.SendMessageToClient("pd", "/unity/X", 1);
+		}
+		else if (Input.GetKeyDown(KeyCode.JoystickButton3)) {
+			Debug.Log("Y pressed");
+			OSCHandler.Instance.SendMessageToClient("pd", "/unity/Y", 1);
+		}
+
 	}
 
     // Update is called once per frame
@@ -70,4 +78,8 @@ public class PdHandler : MonoBehaviour
 
 		// Send message to Pd by OSCHandler.Instance.SendMessageToClient("pd", "/unity/<object name>", count * 20 + 50);
     }
+
+	void onApplicationQuit() {
+		OSCHandler.Instance.SendMessageToClient("pd", "/unity/death", 1);
+	}
 }
